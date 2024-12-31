54954cd3e1cf7c53d24ac59d772a26858f4e4eb7
DerThorsten <derthorstenbeier@gmail.com>
2024-12-31 16:55:20 +0100

    script

--- build_box2d.sh
+++ build_box2d.sh
@@ -3,20 +3,27 @@
 # this dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#  try to remove the old build
-rm -rf $DIR/deps/box2d
+COMMIT_HASH=f377034920c42a26cd498c0a0b1b2e9f2b064989
 
-mkdir -p $DIR/deps/box2d &&
-cd $DIR/deps &&
-curl -L -o box2d-commit.tar.gz https://github.com/erincatto/box2d/archive/f377034920c42a26cd498c0a0b1b2e9f2b064989.tar.gz &&
-tar --strip-components=1 -xvzf box2d-commit.tar.gz -C box2d &&
-cd box2d &&
+#  try to remove the old build
+rm -rf $DIR/deps/box2d/
+mkdir -p $DIR/deps/box2d/build
+
+cd $DIR/deps
+curl -L -o box2d-commit.tar.gz \
+    https://github.com/erincatto/box2d/archive/$COMMIT_HASH.tar.gz
+
+# extract the archive
+tar --strip-components=1 -xvzf box2d-commit.tar.gz -C box2d
+
+# apply patches
+cd box2d
 patch  -p1 -i ../box2d_patches/conditional_disable_cxx_operators.patch
 
-cd $DIR/deps/box2d &&
 
 
-cmake -S deps/box2d -B $DIR/deps/box2d/build \
+
+cmake -S . -B build \
     -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
     -DCMAKE_INSTALL_LIBDIR=$CONDA_PREFIX/lib \
     -DCMAKE_BUILD_TYPE=Release \
@@ -25,5 +32,5 @@ cmake -S deps/box2d -B $DIR/deps/box2d/build \
     -DBOX2D_BENCHMARKS=OFF \
     -DBOX2D_SAMPLES=OFF
 
-cmake --build $DIR/deps/box2d/build \
+cmake --build build \
       --config Release --target install
