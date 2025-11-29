278e5c8beb5053f29791fc8c9979b47a78bfeee5
DerThorsten <derthorstenbeier@gmail.com>
2025-11-29 11:17:11 +0100

    updated kernel-spec

--- scripts/examples_to_notebooks.sh
+++ scripts/examples_to_notebooks.sh
@@ -15,7 +15,7 @@ for file in pyb2d3_samples/*.py; do
         continue
     fi
     base="${filename%.py}"
-    jupytext "$file" --to notebook --output "notebooks/${base}.ipynb"  --update-metadata '{"kernelspec": {"name": "xpython"}}'
+    jupytext "$file" --to notebook --output "notebooks/${base}.ipynb"  --update-metadata '{"kernelspec": {"name": "xpython", "display_name": "Python 3.13 (XPython)","language": "python"}}'
 done
 
 cp -r pyb2d3_samples/examples_common/ notebooks/examples_common
